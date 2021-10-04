c1154dcfd4726b277dbe701a10abed490aaf1afe
Jaden Turnbull <jaden.turnbull@gmail.com>
2021-10-04 12:18:31 +0200

    Todo list

--- Project3/ToDoList/src/main/java/com/example/todolist/datamodel/TodoData.java
+++ Project3/ToDoList/src/main/java/com/example/todolist/datamodel/TodoData.java
@@ -1,6 +1,7 @@
 package com.example.todolist.datamodel;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -11,13 +12,13 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
-import java.util.List;
+
 
 public class TodoData {
     private static TodoData instance = new TodoData();
     private static String filename = "TodoListItems.txt";
 
-    private List<TodoItem> todoItems;
+    private ObservableList<TodoItem> todoItems;
     private DateTimeFormatter formatter;
 
     public static TodoData getInstance() {
@@ -28,14 +29,14 @@ public class TodoData {
         formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     }
 
-    public List<TodoItem> getTodoItems() {
+    public ObservableList<TodoItem> getTodoItems() {
         return todoItems;
     }
 
-    //    public void setTodoItems(List<TodoItem> todoItems) {
-//        this.todoItems = todoItems;
-//    }
-//
+    public void addTodoItem(TodoItem item) {
+        todoItems.add(item);
+    }
+
     public void loadTodoItems() throws IOException {
 
         todoItems = FXCollections.observableArrayList();
@@ -84,13 +85,10 @@ public class TodoData {
                 bw.close();
             }
         }
-
-
-
-
-
-
-
-
     }
+
+    public void deleteTodoItem(TodoItem item) {
+        todoItems.remove(item);
+    }
+
 }
